44d3af82ab30004e7afabadaac5ec33dd1e27b0d
Daogang Tang <daogangtang@gmail.com>
2011-04-18 10:09:17 +0800

    modified on model definitions

--- models/page.lua
+++ models/page.lua
@@ -10,26 +10,26 @@ Page = Node:extend {
 	__name = 'Page';
 	__desc = 'Abstract page node definition.';
 	__fields = {
-		['id'] 		= 	{},						-- 页面的id
-		['name'] 	= 	{},						-- 页面的内部名称
-		['rank'] 	= 	{},						-- 页面在整个页面树中的级别，为字符串
-		['title'] 	= 	{},						-- 页面标题
-		['content'] = 	{},				-- 页面内容
 		['cmd_content'] = { newfield=true, }, 			-- 客户端插件配置输入内容
 		
-		['is_category'] = {},			-- 标明此页面是否是一个类别页面，即是否可接子页面
-		['parent'] 		= {},						-- 页面的父页面id，如果为空，则表明本页面为顶级页面
-		['children'] 	= {},					-- 此页面的子页面id列表字符串，受is_category控制
-		['groups'] 		= {},						-- 此页面可以所属的组，近似就是它们所说的tag
+		['name'] 	= 	{},
+		['rank'] 	= 	{},
+		['title'] 	= 	{ required=true},
+		['content'] = 	{ required=true},
 
-		['comments'] 	= {},					-- 对此页面的评论id列表字符串
-		['attachments'] = {},				-- 附着在此页面上的文件
+		['is_category'] = {},
+		['parent'] 		= { st='ONE', foreign='Page'},
+		['children'] 	= { st='MANY', foreign='Page'},
+		['groups'] 		= { st='MANY', foreign='Page'},
 
-		['created_date'] 	= {},				-- 本页面创建的日期
-		['lastmodified_date'] 	= {},		-- 最后一次修改的日期
-		['creator'] 		= {},					-- 本页面的创建者
-		['owner'] 			= {},					-- 本页面的拥有者
-		['lastmodifier'] 	= {},				-- 最后一次本页面的修改者
+		['comments'] 	= { st='MANY', foreign='Message'},
+		['attachments'] = { st='MANY', foreign='Upload'},
+
+		['created_date'] 	= {},
+		['lastmodified_date'] 	= {},
+		['creator'] 		= { st='ONE', foreign='User'},
+		['owner'] 			= { st='ONE', foreign='User'},
+		['lastmodifier'] 	= { st='ONE', foreign='User'},
 		
 	};
 	
